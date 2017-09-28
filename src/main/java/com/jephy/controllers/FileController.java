e835ce87a1380dfa093facd7e208accc6a2eb0a6
chenshijue <chenshijue@zhixuanhuaxue.com>
2017-09-28 15:21:28 +0800

    添加上传功能

---
+++ src/main/java/com/jephy/controllers/FileController.java
@@ -0,0 +1,28 @@
+package com.jephy.controllers;
+
+import com.jephy.services.storage.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Created by chenshijue on 2017/9/28.
+ */
+
+@RestController
+@RequestMapping("/files")
+public class FileController {
+
+    @Autowired
+    private StorageService storageService;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String uploadFile(@RequestParam("file") MultipartFile file){
+        storageService.store(file);
+        return storageService.getRootLocation().toString();
+    }
+
+}
